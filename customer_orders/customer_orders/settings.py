5453b93ed31b782e9dc47414c926861338eff591
bion-slmn <bionsol25@gmail.com>
2024-06-25 11:15:00 +0300

    configuring database for CI

--- customer_orders/customer_orders/settings.py
+++ customer_orders/customer_orders/settings.py
@@ -88,6 +88,17 @@ WSGI_APPLICATION = 'customer_orders.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 DATABASES = {
     'default': {
